3c92ef9d6e5bdc851404d38c58a2854ec2b3487b
Andrew Bagwell <abagwell90@gmail.com>
2019-02-26 21:25:12 -0500

    adding burger app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Ham with Blue Cheese');
+INSERT INTO burgers (name) VALUES ('Shake Shack Burger');
+INSERT INTO burgers (name) VALUES ('5 guys with pickles Burger');
